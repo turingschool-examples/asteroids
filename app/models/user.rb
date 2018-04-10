db6d8b7a0156b3e0570121a61b14f3b1801abda9
Josh Mejia <joshua.mejia@gmail.com>
2018-04-10 15:12:36 -0600

    Add favorites relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_one :api_key
+  has_many :favorites
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 uncle_jesse = User.create(name: "Jesse Katsopolis", email: "uncle.jesse@example.com")
 uncle_jesse.create_api_key(value: "abc123")
+uncle_jesse.favorites.create(neo_reference_id: "2153306")
